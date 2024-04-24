ea9b4d60a577dc11abb0eec787ad20f029dc6f6e
Jacinto Mba Cantero <jacmba@gmail.com>
2024-04-24 16:08:28 +0100

    Hello controller

---
+++ src/main/java/net/jazbelt/springboot3webapp/hello/HelloController.java
@@ -0,0 +1,32 @@
+package net.jazbelt.springboot3webapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/say-hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello webapp!";
+    }
+
+    @GetMapping
+    @ResponseBody
+    public String htmlHello() {
+        return """
+                <!doctype heml>
+                <html>
+                    <head>
+                        <title>Webapp test HTML</title>
+                    <body>
+                    <h1>Hello world from HTML!</h1>
+                    </body>
+                </html>
+                """;
+    }
+}
