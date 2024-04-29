09a28bd77471a3eae58454328cf6aec98bc592dd
Jacinto Mba Cantero <jacmba@gmail.com>
2024-04-29 12:53:13 +0100

    Integrating H2 db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (id, username, description, target_date, done)
+values (10001, 'john', 'Learn AWS', current_date(), false);
+
+insert into todo (id, username, description, target_date, done)
+values (10002, 'john', 'Learn GCP', current_date(), false);
+
+insert into todo (id, username, description, target_date, done)
+values (10003, 'jack', 'Learn Full Stack', current_date(), false);
+
+insert into todo (id, username, description, target_date, done)
+values (10004, 'jane', 'Learn cloud', current_date(), false);
